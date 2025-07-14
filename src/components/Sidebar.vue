5f737b0acbe03005fa9b5658e543ffd5f2ab6974
Fatur Muhammad <fatur_muhammad@bk.ru>
2025-07-14 23:33:46 +0700

    fix with children sidebar menu

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,15 +1,21 @@
 <template>
     <p class="text-gray-500 font-semibold mb-2">NAVIGATION</p>
     <ul class="menu space-y-1 w-full">
-        <li v-for="item in menu.items" :key="item.label">
+        <li v-for="(item, index) in menu.items" :key="index">
+            <details  v-if="item.children" open>
+                <summary>{{ item.label }}</summary>
+                <ul class="ml-4 mt-1 space-y-1">
+                    <li v-for="(child, idx) in item.children" :key="idx">
+                        <RouterLink :to="child.route">{{ child.label }}</RouterLink>
+                    </li>
+                </ul>
+            </details>
             <RouterLink
-            :to="item.to"
-            class="w-full"
-            :class="{
-                'menu-active': isActive(item.to),
-            }"
+                v-else
+                :class="{ 'menu-active': isActive(item.route) }"
+                :to="item.route"
             >
-            <i :class="`${item.icon} mr-2`"></i> {{ item.label }}
+                <i :class="item.icon + ' mr-2'"></i>{{ item.label }}
             </RouterLink>
         </li>
     </ul>
--- src/stores/menu.ts
+++ src/stores/menu.ts
@@ -1,14 +1,34 @@
+// stores/menu.ts
 import { defineStore } from 'pinia'
 
 export const useMenuStore = defineStore('menu', {
     state: () => ({
         items: [
-        { label: 'Dashboard', icon: 'ph ph-gauge', to: '/home' },
-        { label: 'Analytics', icon: 'ph ph-chart-bar', to: '/analytics' },
-        { label: 'Users', icon: 'ph ph-users', to: '/users' },
-        { label: 'Reports', icon: 'ph ph-file-text', to: '/reports' },
-        { label: 'Messages', icon: 'ph ph-chat-circle-text', to: '/messages' },
-        { label: 'Calendar', icon: 'ph ph-calendar-blank', to: '/calendar' },
+            {
+                label: 'Dashboard',
+                icon: 'ph ph-gauge',
+                route: '/home'
+            },
+            {
+                label: 'Analytics',
+                icon: 'ph ph-chart-bar',
+                route: '/analytics'
+            },
+            {
+                label: 'Users',
+                icon: 'ph ph-users',
+                children: [
+                    { label: 'All Users', route: '/users' },
+                    { label: 'Roles', route: '/users/roles' },
+                ]
+            },
+            {
+                label: 'Reports',
+                icon: 'ph ph-file-text',
+                route: '/reports'
+            },
+            { label: 'Messages', icon: 'ph ph-chat-circle-text', route: '/messages' },
+            { label: 'Calendar', icon: 'ph ph-calendar-blank', route: '/calendar' },
         ]
     })
 })
